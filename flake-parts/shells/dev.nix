dce164f4edb680550ebe6f9731c60eec4b163b83
tsandrini <tomas.sandrini@seznam.cz>
2025-06-22 20:42:06 +0200

    refactor(shells): cleanup defaults from flake-parts-builder

--- flake-parts/shells/dev.nix
+++ flake-parts/shells/dev.nix
@@ -9,8 +9,6 @@
   nixfmt-rfc-style,
   writeShellScriptBin,
   nix-fast-build,
-  treefmt-wrapper ? null,
-  dev-process ? null,
   pre-commit ? null,
   uv,
   gh,
@@ -31,34 +29,30 @@ let
 in
 mkShell {
 
-  packages =
-    (lib.attrValues scripts)
-    ++ (lib.optional (treefmt-wrapper != null) treefmt-wrapper)
-    ++ (lib.optional (dev-process != null) dev-process)
-    ++ [
-      # -- NIX UTILS --
-      nil # Yet another language server for Nix
-      statix # Lints and suggestions for the nix programming language
-      deadnix # Find and remove unused code in .nix source files
-      nix-output-monitor # Processes output of Nix commands to show helpful and pretty information
-      nixfmt-rfc-style # An opinionated formatter for Nix
-      nix-fast-build # A tool to speed up Nix builds by caching build results
+  packages = (lib.attrValues scripts) ++ [
+    # -- NIX UTILS --
+    nil # Yet another language server for Nix
+    statix # Lints and suggestions for the nix programming language
+    deadnix # Find and remove unused code in .nix source files
+    nix-output-monitor # Processes output of Nix commands to show helpful and pretty information
+    nixfmt-rfc-style # An opinionated formatter for Nix
+    nix-fast-build # A tool to speed up Nix builds by caching build results
 
-      # -- GIT RELATED UTILS --
-      commitizen # Tool to create committing rules for projects, auto bump versions, and generate changelogs
-      cz-cli # The commitizen command line utility
-      # fh # The official FlakeHub CLI
-      gh # GitHub CLI tool
-      # gh-dash # Github Cli extension to display a dashboard with pull requests and issues
+    # -- GIT RELATED UTILS --
+    commitizen # Tool to create committing rules for projects, auto bump versions, and generate changelogs
+    cz-cli # The commitizen command line utility
+    # fh # The official FlakeHub CLI
+    gh # GitHub CLI tool
+    # gh-dash # Github Cli extension to display a dashboard with pull requests and issues
 
-      # -- BASE LANG UTILS --
-      # markdownlint-cli # Command line interface for MarkdownLint
-      # nodePackages.prettier # Prettier is an opinionated code formatter
-      # typos # Source code spell checker
+    # -- BASE LANG UTILS --
+    # markdownlint-cli # Command line interface for MarkdownLint
+    # nodePackages.prettier # Prettier is an opinionated code formatter
+    # typos # Source code spell checker
 
-      # -- (YOUR) EXTRA PKGS --
-      uv # Extremely fast Python package installer and resolver, written in Rust
-    ];
+    # -- (YOUR) EXTRA PKGS --
+    uv # Extremely fast Python package installer and resolver, written in Rust
+  ];
 
   shellHook = ''
     ${lib.concatLines (lib.mapAttrsToList (name: value: "export ${name}=${value}") env)}
